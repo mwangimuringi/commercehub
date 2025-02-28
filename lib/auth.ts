a6a8b5fca8e8751a7bdaca29f171c9f69f2dc6ef
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-28 22:05:46 +0300

    Implement Clerk middleware for API authentication.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 // src/lib/auth.ts
-import { Clerk } from "@clerk/clerk-sdk-node";
+import { Clerk, WithAuth } from "@clerk/clerk-sdk-node";
 
 const clerk = new Clerk({ secretKey: process.env.CLERK_SECRET_KEY! });
 
+export const withAuth: WithAuth = clerk.withAuth();
 export default clerk;
