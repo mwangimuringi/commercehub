4020eec40f7cfb2eac5768e82f85212d5d09f452
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-01 20:33:36 +0300

    Added login functionality in user store.

--- store/userStore.ts
+++ store/userStore.ts
@@ -5,3 +5,15 @@ interface UserState {
   user: User | null;
   setUser: (user: User | null) => void;
 }
+
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+    }),
+    {
+      name: "user-storage", // Persist user data in localStorage
+    }
+  )
+);
